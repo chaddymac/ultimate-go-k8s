5f2493b081aced15f66b708321f3dc0cac942183
chaddymac <moodychadiamond@yahoo.com>
2023-04-12 10:20:06 -0400

    add logger and sales api

---
+++ app/services/sales_api/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/chaddymac/ultimate-go/foundation/logger"
+	"go.uber.org/zap"
+)
+
+func main() {
+	log, err := logger.New("sales-api")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer log.Sync()
+
+	if err := run(log); err != nil {
+		log.Errorw("startup", "error", err)
+		log.Sync()
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+
+	// ==============================================================
+	// GOMAXPROCS
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+	return nil
+}
---
+++ makefile
@@ -0,0 +1,7 @@
+
+run:
+	go run app/services/sales_api/main.go
+
+tidy:
+	go mod tidy 
+	go mod vendor
